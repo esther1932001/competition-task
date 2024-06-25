5143fb8dbebd5f68f28db0cb9b16319813bb613f
esther1932001 <Esther.20367591@compit.aun.edu.eg>
2024-06-25 08:01:29 +0300

    add NNset

--- Task/CompetationDetailsCoreData/CompetationDetailsCD+CoreDataProperties.swift
+++ Task/CompetationDetailsCoreData/CompetationDetailsCD+CoreDataProperties.swift
@@ -4,7 +4,6 @@
 //
 //  Created by esterelzek on 23/06/2024.
 //
-
 import Foundation
 import CoreData
 
@@ -24,4 +23,38 @@ extension CompetationDetailsCD {
     // Relationships
     @NSManaged public var area: AreaCD?
     @NSManaged public var currentSeason: SeasonCD?
+    @NSManaged public var teams: NSSet?      // Assume this relationship exists
+    @NSManaged public var matches: NSSet?    // Assume this relationship exists
+}
+
+// MARK: Generated accessors for teams
+extension CompetationDetailsCD {
+
+    @objc(addTeamsObject:)
+    @NSManaged public func addToTeams(_ value: TeamCD)
+
+    @objc(removeTeamsObject:)
+    @NSManaged public func removeFromTeams(_ value: TeamCD)
+
+    @objc(addTeams:)
+    @NSManaged public func addToTeams(_ values: NSSet)
+
+    @objc(removeTeams:)
+    @NSManaged public func removeFromTeams(_ values: NSSet)
+}
+
+// MARK: Generated accessors for matches
+extension CompetationDetailsCD {
+
+    @objc(addMatchesObject:)
+    @NSManaged public func addToSeasons(_ value: SeasonCD)
+
+    @objc(removeMatchesObject:)
+    @NSManaged public func removeFromSeasons(_ value: SeasonCD)
+
+    @objc(addMatches:)
+    @NSManaged public func addToSeasons(_ values: NSSet)
+
+    @objc(removeMatches:)
+    @NSManaged public func removeFromSeasons(_ values: NSSet)
 }
